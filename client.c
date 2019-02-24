573be07bb39f9bbcbf6c728f1f418d64533ac34e
horno <i.palacin.a@gmail.com>
2019-02-24 18:29:39 +0100

    -c finnished

--- client.c
+++ client.c
@@ -1,14 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+#include <string.h>
 
 
 /* Main function */
 int main(int argc, char *argv[])
 {
+	char cfg_file[] = "client.cfg";
 
-	/* -c on argument */
-	if()
 
+
+
+	/* argument functionalities */
+	if(argc > 1)
+	{
+		if(strcmp(argv[1],"-c") == 0)
+		{
+			if(argc != 3)
+			{	
+				printf("Use:\n");
+				printf("\t%s -c <nom_arxiu>\n",argv[0]);
+				exit(-1);
+			}
+			else
+			{
+				sprintf(cfg_file, argv[2]);
+			}
+		}
+	}
+	
+	return 0;
 }
+
+
+
+
+
+
