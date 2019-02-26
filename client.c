76a97a512aaded2d6fb6c669240cf8c4101aee85
horno <i.palacin.a@gmail.com>
2019-02-26 01:53:42 +0100

    collect_config_data working

--- client.c
+++ client.c
@@ -4,21 +4,25 @@
 #include <string.h>
 
 
+/* Structures */
+struct cfg_data{
+	char nom_equip[7];
+	char MAC_equip[13];
+	char nom_server[20];//TODO Pot ser més gran?
+	int port_server;
+};
+
+
+/* Function declarations */
+struct cfg_data collect_config_data(char cfg_file[]);
+
+
 /* Main function */
 int main(int argc, char *argv[])
 {
 	char cfg_file[] = "client.cfg";
 
-	FILE *fpointer;
-	char singleLine[100];
-       	char *cfg_param; //TODO: millorar	
-
-	char nom_equip[7];
-	char MAC_equip[13];
-	char nom_server[20]; //TODO: Pot ser més gran?
-	int port_server;
-
-
+	struct cfg_data dataconfig;
 
 	/* argument functionalities */
 	if(argc > 1)
@@ -38,55 +42,52 @@ int main(int argc, char *argv[])
 		}
 	}
 	
-	// Obtain information from the configuration file
-	fpointer = fopen(cfg_file,"r");
-	while(fgets(singleLine,100,fpointer) != NULL)
-	{
-		cfg_param = strtok(singleLine," ");		
-		if(strcmp(cfg_param,"nom") == 0)
-		{
-			cfg_param = strtok(NULL,"\n");
-			strcpy(nom_equip, cfg_param);
-		}
-		else if(strcmp(cfg_param,"MAC") == 0)
-		{
-			cfg_param = strtok(NULL,"\n");
-                        strcpy(MAC_equip,cfg_param);
-		}
-		else if(strcmp(cfg_param,"Server") == 0)
-		{
-			cfg_param = strtok(NULL,"\n");
-                        strcpy(nom_server,cfg_param);
-		}
-		else if(strcmp(cfg_param,"Server-port") == 0)
-		{
-			cfg_param = strtok(NULL,"\n");
-                        port_server = atoi(cfg_param);
-		}	
-		else
-		{
-			printf("Error in the content of %s\n",cfg_file );
-			printf("Terminating process\n");
-			exit(-1);
-		}
-	}
-        printf("Nom equip: %s\n",nom_equip);
-	printf("MAC equip: %s\n",MAC_equip);
-        printf("Nom servidor: %s\n",nom_server);
-        printf("Port servidor: %d\n",port_server);
-	
-	fclose(fpointer);
-	
-
-
-
-
+	dataconfig = collect_config_data(cfg_file);
+	printf("%s\n",dataconfig.nom_equip);
+	printf("%s\n",dataconfig.MAC_equip);
+	printf("%s\n",dataconfig.nom_server);
+	printf("%d\n",dataconfig.port_server);
 
 	return 0;
 }
 
 
+struct cfg_data collect_config_data(char cfg_file[])
+{
+	// Obtain information from the configuration file
+	FILE *fpointer;
+        char singleLine[100];
+        char *cfg_param; //TODO: millorar  
 
+	struct cfg_data dataconfig;
 
-
-
+	fpointer = fopen(cfg_file,"r");
+        while(fgets(singleLine,100,fpointer) != NULL)
+        {
+                cfg_param = strtok(singleLine," ");             
+                if(strcmp(cfg_param,"nom") == 0)
+                {
+                        strcpy(dataconfig.nom_equip, strtok(NULL,"\n"));
+	       	}
+                else if(strcmp(cfg_param,"MAC") == 0)
+                {
+                        strcpy(dataconfig.MAC_equip,strtok(NULL,"\n"));
+                }
+                else if(strcmp(cfg_param,"Server") == 0)
+                {
+                        strcpy(dataconfig.nom_server,strtok(NULL,"\n"));
+                }
+                else if(strcmp(cfg_param,"Server-port") == 0)
+                {
+                        dataconfig.port_server = atoi(strtok(NULL,"\n"));
+                }       
+                else
+                {
+                        printf("Error in the content of %s\n",cfg_file );
+                        printf("Terminating process\n");
+                        exit(-1);
+                }
+	}
+        fclose(fpointer);
+	return dataconfig;
+}
