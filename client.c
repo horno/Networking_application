21df67ac310b2ae2f9beea239ff2288fa803c8f1
horno <i.palacin.a@gmail.com>
2019-03-03 02:19:32 +0100

    Auxilliary function -fill_structures- not working, core dumped

--- client.c
+++ client.c
@@ -30,7 +30,10 @@ struct register_package{
 /* Function declarations */
 struct cfg_data collect_config_data(char cfg_file[]);
 char* change_cfg_filename(int argc, char *argv[]);
-int socket_work();
+void fill_structures(int sock, struct sockaddr_in *addr_cli,
+               struct cfg_data dataconfig, struct hostent *ent,
+               struct sockaddr_in *addr_server,
+               struct register_package *register_pack);
 
 /* Main function */
 int main(int argc, char *argv[])
@@ -39,7 +42,7 @@ int main(int argc, char *argv[])
 
 	struct cfg_data dataconfig;
 	struct sockaddr_in	addr_cli,addr_server;
-	struct hostent *ent;
+	struct hostent *ent = malloc(sizeof(*ent));
 
 	struct register_package register_pack;
 
@@ -73,13 +76,18 @@ int main(int argc, char *argv[])
 		perror("Error al obrir el socket:");
 		exit(-1);
 	}
-		
-	/* Fills the client address struct for the binding */
-	/* TO DO: mirar si fer memset */
+
+	fill_structures(sock, &addr_cli, dataconfig, ent, &addr_server,
+			&register_pack);
+/* ----------------------------------------------------- */
+/*
+
+	/x* Fills the client address struct for the binding *x/
+	/x* TO DO: mirar si fer memset *x/
 	addr_cli.sin_family = AF_INET;
 	addr_cli.sin_addr.s_addr = htonl(INADDR_ANY);
 	addr_cli.sin_port = htons(0);
-	/* Binding */
+	/x* Binding *x/
 	if(bind(sock, (struct sockaddr *)&addr_cli,
 	               sizeof(struct sockaddr_in))<0)
 	{
@@ -87,25 +95,28 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	/* Gets the IP of the host by its name */
+	/x* Gets the IP of the host by its name *x/
 	ent = gethostbyname(dataconfig.nom_server);
 
-	/* Fills the server address struct where we send the data */
-	/* TO DO: mirar si fer memset */
+	/x* Fills the server address struct where we send the data *x/
+	/x* TO DO: mirar si fer memset *x/
 	addr_server.sin_family = AF_INET;
 	addr_server.sin_addr.s_addr = (((struct in_addr *)ent->h_addr)
 					->s_addr);
 	addr_server.sin_port = htons(dataconfig.port_server);
 	
-	/* Fills register package */
+	/x* Fills register package *x/
 	memset(&register_pack,0,sizeof(register_pack));
-	register_pack.tipus_paquet = '0';
+	register_pack.tipus_paquet = 0x00;
 	strcpy(register_pack.nom_equip,dataconfig.nom_equip);
 	strcpy(register_pack.MAC_addr,dataconfig.MAC_equip);
 	strcpy(register_pack.num_aleatori,"");
 	strcpy(register_pack.dades,"");
 	
-	
+*/
+/* ------------------------------------------  */
+
+
 	/* Sends package to the server */  
 	a = sendto(sock,&register_pack,sizeof(register_pack)+1,0, 
 	       	(struct sockaddr*) &addr_server, sizeof(addr_server));
@@ -134,14 +145,44 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-/*int socket_work()
+void fill_structures(int sock, struct sockaddr_in *addr_cli,
+	       struct cfg_data dataconfig, struct hostent *ent,
+       	       struct sockaddr_in *addr_server,
+	       struct register_package *register_pack)
 {
+	/* Fills the client address struct for the binding */
+        /* TO DO: mirar si fer memset */
+        addr_cli->sin_family = AF_INET;
+        addr_cli->sin_addr.s_addr = htonl(INADDR_ANY);
+        addr_cli->sin_port = htons(0);
+        /* Binding */
+        if(bind(sock, (struct sockaddr *)&addr_cli,
+                       sizeof(struct sockaddr_in))<0)
+        {
+                perror("Error amb el binding del socket:");
+                exit(-1);
+        }
 
+        /* Gets the IP of the host by its name */
 
+        ent = gethostbyname(dataconfig.nom_server);
 
+        /* Fills the server address struct where we send the data */
+        /* TO DO: mirar si fer memset */
+        addr_server->sin_family = AF_INET;
+        addr_server->sin_addr.s_addr = (((struct in_addr *)ent->h_addr)
+                                        ->s_addr);
+        addr_server->sin_port = htons(dataconfig.port_server);
 
+        /* Fills register package */
+        memset(&register_pack,0,sizeof(register_pack));
+        register_pack->tipus_paquet = 0x00;
+        strcpy(register_pack->nom_equip,dataconfig.nom_equip);
+        strcpy(register_pack->MAC_addr,dataconfig.MAC_equip);
+        strcpy(register_pack->num_aleatori,"");
+        strcpy(register_pack->dades,"");
 
-}*/
+}
 
 char* change_cfg_filename(int argc, char *argv[])
 {
